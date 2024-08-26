5cc841ddcd4410daa14d39a13e14d512b8960577
Bradford Wagner <wagner.bradford@gmail.com>
2024-08-26 15:23:48 -0400

    feat(cmd): main to jump off (#12)

--- cmd/ks/main.go
+++ cmd/ks/main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/bradfordwagner/go-util/bwutil"
 	"github.com/bradfordwagner/go-util/flag_helper"
 	"github.com/bradfordwagner/go-util/log"
 	"github.com/bradfordwagner/ks/internal/args"
+	"github.com/bradfordwagner/ks/internal/choose"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"os"
 	"time"
 )
 
-var rootCmd = &cobra.Command{
-	Use: "ks",
-}
+var commands []*cobra.Command
 
 func init() {
-	rootCmd.AddCommand(
+	commands = []*cobra.Command{
 		linkCmd,
 		pipeCmd,
 		resourceCmd,
 		resourceLoadCommand,
 		tmuxMultiCmd,
 		tmuxWindowCmd,
-	)
+	}
+	rootCmd.AddCommand(commands...)
 }
 
 func standardFlagsInit(fs *pflag.FlagSet) {
@@ -41,4 +42,23 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		l.With("error", err).Fatal("could not execute command")
 	}
+}
+
+var rootCmd = &cobra.Command{
+	Use: "ks",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		// map command names to commands
+		nameToCommand := make(map[string]*cobra.Command)
+		for _, command := range commands {
+			nameToCommand[command.Name()] = command
+		}
+
+		sortedNames := bwutil.MapKeys(nameToCommand)
+		selectedCommand, err := choose.One(sortedNames)
+		if err != nil {
+			return err
+		}
+
+		return nameToCommand[selectedCommand].RunE(cmd, args)
+	},
 }
